c9f4c7163e5e8a12774d181fcbc133d78dc2ec0c
Thomas Knickman <tknickman@gmail.com>
2025-06-05 09:48:41 -0400

    test

--- actions/status/dist/utils/get-state.js
+++ actions/status/dist/utils/get-state.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.isPostStep = isPostStep;
 const POST_STEP_ENV_KEY = "STATE_isPost";
 function isPostStep() {
+    console.log(process.env);
     return process.env.GITHUB_ACTION_STEP_STATE === POST_STEP_ENV_KEY;
 }
